090c7ca47801dac890bae299dcf10824863fc257
Mahiimaa <137261863+Mahiimaa@users.noreply.github.com>
2024-06-23 12:06:22 +0545

    Signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Login />} />
+          <Route path="/signup" element={<Signup/>} />
           <Route path="/" element={<Login />} />
         </Routes>
       </BrowserRouter>
