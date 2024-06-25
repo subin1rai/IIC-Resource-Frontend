a9252a68b52808d39f548d89277223474ae3ccd7
Lizan7 <lizanstha777@gmail.com>
2024-06-25 13:00:09 +0545

    Ma lizan rai

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import "./App.css";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
