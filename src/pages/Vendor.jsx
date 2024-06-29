edaf091997d19cf7ae28734d65d9c4f961093995
Aneer-Thapa1 <anir234thapa@gmail.com>
2024-06-29 22:15:35 +0545

    vendor table

--- src/pages/Vendor.jsx
+++ src/pages/Vendor.jsx
@@ -7,100 +7,8 @@ import filterIcon from "../assets/filter.svg";
 import validVendor from "../assets/user.svg";
 import close from "../assets/close.svg";
 import axios from "axios";
+import VendorTable from "../components/VendorTable";
 const Vendor = () => {
-  const [data, setData] = useState([
-    {
-      name: "Richard Martin",
-      vat: "7687764556",
-      contact: "7687764556",
-      amount: 20000,
-      date: "6/25/2024",
-      duration: "15 days",
-      status: "Complete",
-    },
-    {
-      name: "Tom Homan",
-      vat: "9867545368",
-      contact: "9867545368",
-      amount: 20000,
-      date: "6/25/2024",
-      duration: "15 days",
-      status: "Complete",
-    },
-    {
-      name: "Veandir",
-      vat: "9867545566",
-      contact: "9867545566",
-      amount: 20000,
-      date: "6/25/2024",
-      duration: "15 days",
-      status: "Complete",
-    },
-    {
-      name: "Charin",
-      vat: "9267545457",
-      contact: "9267545457",
-      amount: 20000,
-      date: "6/25/2024",
-      duration: "15 days",
-      status: "Complete",
-    },
-    {
-      name: "Hoffman",
-      vat: "9367546531",
-      contact: "9367546531",
-      amount: 20000,
-      date: "6/25/2024",
-      duration: "15 days",
-      status: "Complete",
-    },
-    {
-      name: "Fainden Juke",
-      vat: "9667545982",
-      contact: "9667545982",
-      amount: 20000,
-      date: "6/25/2024",
-      duration: "15 days",
-      status: "Pending",
-    },
-    {
-      name: "Martin",
-      vat: "9867545457",
-      contact: "9867545457",
-      amount: 20000,
-      date: "6/25/2024",
-      duration: "15 days",
-      status: "Complete",
-    },
-    {
-      name: "Joe Nike",
-      vat: "9567545769",
-      contact: "9567545769",
-      amount: 20000,
-      date: "6/25/2024",
-      duration: "15 days",
-      status: "Complete",
-    },
-    {
-      name: "Dender Luke",
-      vat: "9667545980",
-      contact: "9667545980",
-      amount: 20000,
-      date: "6/25/2024",
-      duration: "15 days",
-      status: "Pending",
-    },
-    {
-      name: "Martin",
-      vat: "9867545457",
-      contact: "9867545457",
-      amount: 20000,
-      date: "6/25/2024",
-      duration: "15 days",
-      status: "Complete",
-    },
-  ]);
-
   const [vendor, setVendor] = useState({
     vendor_name: "",
     vendor_vat: "",
@@ -164,6 +72,7 @@ const Vendor = () => {
             </div>
           </div>
         </div>
+
         <div className="up">
           <div className="top">
             <h3> Vendors </h3>
@@ -181,39 +90,8 @@ const Vendor = () => {
               </div>
             </div>
           </div>
-          <table className="vendor-table">
-            <thead>
-              <tr>
-                <th>Vendor Name</th>
-                <th>VAT No</th>
-                <th>Contact Number</th>
-                <th>Purchase Amount</th>
-                <th>Recent Purchase</th>
-                <th>Payment Duration</th>
-                <th>Payment Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((vendor, index) => (
-                <tr key={index}>
-                  <td>{vendor.name}</td>
-                  <td>{vendor.vat}</td>
-                  <td>{vendor.contact}</td>
-                  <td>{vendor.amount}</td>
-                  <td>{vendor.date}</td>
-                  <td>{vendor.duration}</td>
-                  <td
-                    className={
-                      vendor.status === "Complete" ? "complete" : "pending"
-                    }
-                  >
-                    {vendor.status}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <div className="last">
+          <VendorTable />
+          {/* <div className="last">
             <div className="previous">
               <button>Previous </button>
             </div>
@@ -221,7 +99,7 @@ const Vendor = () => {
             <div className="next">
               <button> Next </button>
             </div>
-          </div>
+          </div> */}
         </div>
 
         {addFormVisibility && (
