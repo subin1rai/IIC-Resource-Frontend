d71751be4f70fd3abc97bcafc68a3648036debb2
pirates59 <lenishaghimire07@gmail.com>
2024-07-04 09:56:53 +0545

    Category

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -5,7 +5,40 @@ import "../styles/category.css";
 import axios from "axios";
 
 const Category = () => {
-    const [category,setCategory] = useState([])
+    const [category,setCategory] = useState({
+      category_name:"",
+
+    });
+
+    const [addFormVisibility, setAddFormVisibility] = useState(false);
+
+    const displayAddPopup = () => {
+      setAddFormVisibility(true);
+    };
+  
+    const closeCategoryForm = () => {
+      setAddFormVisibility(false);
+    };
+
+    const handleChange = (e) => {
+      setCategory((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+      console.log(e.target.value);
+    };
+  
+    const handlesubmit = async (event) => {
+      event.preventDefault();
+  
+      try {
+        const response = await axios.post(
+          "http://localhost:8898/api/addCategory",
+          category
+        );
+  
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
     useEffect(()=>{},[])
   return (
@@ -13,14 +46,20 @@ const Category = () => {
       <Sidebar />
       <div className="category-main">
         <Topbar />
-        <div className="main">
-          <p className="title">Categories</p>
+        <div className="main-container">
           <div className="first">
-           <div className="head">
-           <p>Category</p>
-           <button>Add Category</button>
+           <div className="category-title">
+           <p> Category</p>
            </div>
-            <div className="tables">
+
+           <div className="icon-action">
+
+            <button className="add-button" onClick={displayAddPopup}>
+                Category
+              </button>
+              </div>
+
+              <div className="tables">
             <table>
               <thead>
                 <tr>
@@ -46,6 +85,39 @@ const Category = () => {
           </div>
         </div>
       </div>
+
+      {addFormVisibility && (
+        <form action="" onSubmit={handlesubmit} className="category-form">
+          <button
+            type="button"
+            className="discard-button"
+            onClick={closeCategoryForm}
+          >
+            <img src={close} alt="" />
+          </button>
+          <p className="title">Add Category</p>
+          <div className="field">
+            <label htmlFor="category_name">Category Name</label>
+            <input
+              type="text"
+              placeholder="Enter category name"
+              name="category_name"
+              id="category_name"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="buttons">
+            <button type="submit" className="add-buttons">
+              Add Category
+            </button>
+          </div>
+        </form>
+      )}
+
+      {addFormVisibility && (
+        <div className="overlay-category" onClick={closeCategoryForm}></div>
+      )}
+
     </div>
   );
 };
