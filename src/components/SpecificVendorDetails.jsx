5ddef03c306208fc9ec2a0486314d1ce36fa00b4
Aneer-Thapa1 <anir234thapa@gmail.com>
2024-07-09 10:12:01 +0545

    Fixed css of add bill

--- src/components/SpecificVendorDetails.jsx
+++ src/components/SpecificVendorDetails.jsx
@@ -3,71 +3,42 @@ import "../styles/details.css";
 import front from "../assets/arrow-right.svg";
 
 const SpecificVendorDetails = () => {
-    // const [vendor, setVendor] = useState({
-    //     vendor_name: "",
-    //     vendor_contact: "",
-    //     purchase_amt: "",
-    //     last_purchase_date: "",
-    //     recent_purchase_date: "",
-    //     last_paid_date: "",
-    //     payment_duration: "",
-    //     tds: "",
-    //     total_payment: "",
-    //     pending_payment: "",
-    //     next_payment_date: "",
-    //     payment_status: ""
-    // }); 
-    
-//     useEffect(() => {
-//         const getAllVendor = async () =>{
-//          console.log("hOOOO")
- 
-//         try {
-            
-//             const response = await axios.get("http://localhost:8898/api/vendor")
-//             console.log(response)
-//         } catch (error) {
-//             console.log(error)
-//         }
-//     }
-//     getAllVendor()
-// },[]);
-
-// console.log("hi")
-    return(
-        <div className="vdetails">
-            <>
-            <div className="title">
-            <h3> Vendors </h3> <img src={front} alt=""></img> <p> Lizan Suppliers</p>
-            </div>
-            <div className="head">
-                    <h1>Lizan Suppliers</h1>
-                    <p>Contact: </p>
-                </div>
-                <hr className="line" />
-                <div className="content">
-                    <div className="left">
-                        <p> VAT Number:  </p>
-                        <p> Purchase Amount: </p>
-                        <p> Last Purchase Date:</p>
-                        <p> Recent Purchase Date:</p>
-                        <p> Last Paid Date: </p>
-                        <p> Payment Duration: </p>
-                        </div>
-                        <div className="right">
-                        <p> TDS: </p>
-                        <p> Total Payment: </p>
-                        <p> Pending Payment: </p>
-                        <p> Next Payment Date: </p>
-                        <p> Payment Status: </p>
-                        </div>
-                    </div></>
-                    <div className="btn">
-                            <button  className= "edit">Edit details</button>
-                            <button className="blacklist" > Add to blacklist </button>
-                    </div>
+  return (
+    <div className="vdetails">
+      <>
+        <div className="title">
+          <h3> Vendors </h3> <img src={front} alt=""></img>{" "}
+          <p> Lizan Suppliers</p>
         </div>
-    );
+        <div className="head">
+          <h1>Lizan Suppliers</h1>
+          <p>Contact: </p>
+        </div>
+        <hr className="line" />
+        <div className="content">
+          <div className="left">
+            <p> VAT Number: </p>
+            <p> Purchase Amount: </p>
+            <p> Last Purchase Date:</p>
+            <p> Recent Purchase Date:</p>
+            <p> Last Paid Date: </p>
+            <p> Payment Duration: </p>
+          </div>
+          <div className="right">
+            <p> TDS: </p>
+            <p> Total Payment: </p>
+            <p> Pending Payment: </p>
+            <p> Next Payment Date: </p>
+            <p> Payment Status: </p>
+          </div>
+        </div>
+      </>
+      <div className="btn">
+        <button className="edit">Edit details</button>
+        <button className="blacklist"> Add to blacklist </button>
+      </div>
+    </div>
+  );
 };
 export default SpecificVendorDetails;
 
