06e8ee39e72894d546a0ca6df63e2c1309dcb700
Aneer-Thapa1 <anir234thapa@gmail.com>
2024-07-14 11:42:04 +0545

    notification

--- src/pages/UserRequest.jsx
+++ src/pages/UserRequest.jsx
@@ -1,25 +1,20 @@
 import React from "react";
 import NavTable from "../components/NavTable";
 // import search from "../assets/search.png";
-import "../styles/userrequest.css";
+import "../styles/userRequest.css";
 import Navbar from "../components/Navbar";
 
-
-const UserRequest = () =>{
-    return(
-        <div className="nav">
-        <Navbar/>
-        <div className="requestcontent">
-            <form action="" className="request-form">
-                <h1>Request Resource</h1>
-                <h5>You can request the resource of your choice</h5>
-
-                
-
-            </form>
-        </div>
-        </div>
-
-    );
-}
+const UserRequest = () => {
+  return (
+    <div className="nav">
+      <Navbar />
+      <div className="requestcontent">
+        <form action="" className="request-form">
+          <h1>Request Resource</h1>
+          <h5>You can request the resource of your choice</h5>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default UserRequest;
