a324e1ada21e409c97cae9c1d439cc96631baeb1
pirates59 <lenishaghimire07@gmail.com>
2024-07-16 15:04:07 +0545

    UserRequest

--- src/pages/UserRequest.jsx
+++ src/pages/UserRequest.jsx
@@ -15,27 +15,29 @@ const UserRequest = () => {
             <h1>Request Resource</h1>
             <h5>You can request the resource of your choice</h5>
           </div>
+
           {/* creating required fields for the form */}
           <div className="singleField">
             <label htmlFor="categoryName">Category Name</label>
             <select name="category_name" id="">
               <option value="">Select a category</option>
-              <option value="">Books</option>
-              <option value="">Pens</option>
+              <option value="Books">Books</option>
+              <option value="Pens">Pens</option>
             </select>
           </div>
           <div className="singleField">
-            <label htmlFor="categoryName">Item Category</label>
+            <label htmlFor="itemCategory">Item Category</label>
             <select name="item_category" id="">
               <option value="">Select a category</option>
-              <option value="">Books</option>
-              <option value="">Pens</option>
-              <option value="">Pencil</option>
+              <option value="Books">Books</option>
+              <option value="Pens">Pens</option>
+              <option value="Pencil">Pencil</option>
             </select>
           </div>
+          
           <div className="singleField">
-            <label htmlFor="categoryName">Quantity</label>
-            <input type="number" name="" id="" placeholder="Enter quantity" />
+            <label htmlFor="quantity">Quantity</label>
+            <input type="number" name="quantity" id="" placeholder="Enter quantity" />
           </div>
           <div className="request-buttons">
             <button type="submit" className="request-btn">
@@ -43,23 +45,36 @@ const UserRequest = () => {
             </button>
           </div>
         </form>
+
+        {/* Vertical line */}
+        <div className="divider"></div>
+
         {/* Creating a div for keeping history records */}
         <div className="requestHistory">
           <div className="heading2">
-            <h1>History</h1>
-            <h5>You can view your history details here.</h5>
-            <button type="filter" className="filter-btn">
-              <img src={filter} alt="" />
-              Filter</button>
-
+            <div>
+              <h1>History</h1>
+              <h5>You can view your request history.</h5>
+            </div>
+            <div className="filter-options">
+              <button className="month-btn">By date</button>
+              <button className="date-btn">By month</button>
+            </div>
           </div>
           <div className="history">
-
+            <div className="history-item">
+              <h4>Item:</h4>
+              <h4>Status:</h4>
+              <h4>Quantity:</h4>
+              <h4>Issued by:</h4>
+              <h4>Requested date:</h4>
+              <h4>Issued date:</h4>
+            </div>
           </div>
         </div>
-
       </div>
     </div>
   );
 };
+
 export default UserRequest;
