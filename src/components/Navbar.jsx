56d431466cc9360b73965c6bb387f4e97a847761
Aneer-Thapa1 <anir234thapa@gmail.com>
2024-07-18 10:58:11 +0545

    managed all tables

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,8 +12,7 @@ const Navbar = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post(" http://localhost:8898/api/login", 
-      );
+      const response = await axios.post(" http://localhost:8898/api/login");
       navigate("/");
       console.log(response);
     } catch (error) {
@@ -21,19 +20,31 @@ const Navbar = () => {
     }
   };
   return (
-        <div className="navbar">
-          <div className="logo">
-            <img src={logo} alt="main logo" />
-          </div>
-          <div className="navright">
-              <NavLink to="/userhome" className = {setActiveClass}> Home </NavLink>
-              <NavLink to ="/userrequest" className = {setActiveClass}> Request </NavLink>
-              <NavLink to ="/aboutus" className = {setActiveClass}> Request History </NavLink>
-            <button className="btn">
-              <NavLink to ="/" onClick={handleLogin} className={setActiveClass}> Login </NavLink>
-            </button>
-          </div>
+    <div className="navbar">
+      <div className="logo">
+        <img src={logo} alt="main logo" />
       </div>
-  );     
-}
+      <div className="navright">
+        <NavLink to="/userhome" className={setActiveClass}>
+          {" "}
+          Home{" "}
+        </NavLink>
+        <NavLink to="/userrequest" className={setActiveClass}>
+          {" "}
+          Request{" "}
+        </NavLink>
+        <NavLink to="/aboutus" className={setActiveClass}>
+          {" "}
+          Request History{" "}
+        </NavLink>
+        <button className="btn">
+          <NavLink to="/" onClick={handleLogin} className={setActiveClass}>
+            {" "}
+            Login{" "}
+          </NavLink>
+        </button>
+      </div>
+    </div>
+  );
+};
 export default Navbar;
