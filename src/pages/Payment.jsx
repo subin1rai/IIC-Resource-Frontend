0f18ccc5a08c72cd945056708ed47624f800678e
Mahiimaa <137261863+Mahiimaa@users.noreply.github.com>
2024-07-19 10:51:44 +0545

    payment page, table and its summary

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -3,25 +3,48 @@ import React, { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import "../styles/payment.css";
 import Topbar from "../components/Topbar";
+import PaymentTable from "../components/PaymentTable";
+import payment from "../assets/user.svg";
+import filterIcon from "../assets/filter.svg";
 import axios from "axios";
 
 const Payment = () => {
+
   return (
     <div className="payment">
     <Sidebar />
     <div className="payment-main">
       <Topbar />
-      <div className="payment-container">
-      <div className="payment-container-top">
-      <div className="payment-title">
+      <div className="flex w-[99.5%] justify-center  h-[16%]">
+      <div className="bg-white w-[99.5%] flex flex-col rounded-xl  p-2 ">
+      <h3 className="font-bold ml-3">Summary</h3>
+      <div className="flex justify-around ">
+      <div className="flex flex-col items-center justify-center gap-3 ">
+                <img src={payment} alt="" />
+                <h4>{payment.length}</h4>
+                <p>Total Pending Payment</p>
+              </div>
         </div>
         </div>
-        
+          </div>
+          <div className=" w-[99.5%] rounded-xl items-center bg-white p-3" >
+          <div className=" flex justify-between w-[99.5%] rounded-xl items-center bg-white">
+            <div className=" font-semibold ml-4">
+              <p>Payments</p>
+            </div>
+            <div className=" flex gap-10">
+              <button className=" flex bg-white-800 border-2 rounded-md p-2 px-5 items-center gap-2 text-grey">
+                {" "}
+                <img src={filterIcon} alt="" />
+                Filters
+              </button>
+            </div>
+          </div>
+          <PaymentTable />
+          </div>
+          </div>
+          </div>
 
-         
-          </div>
-          </div>
-          </div>
   );
 };
 
