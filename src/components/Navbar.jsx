04c9520d3dbc8927a9cc2d4cfb0b08c0b629b631
Aneer-Thapa1 <anir234thapa@gmail.com>
2024-07-22 08:42:50 +0545

    button fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import logo from "../assets/Top.png"
-import userprofile from "../assets/user.svg"
+import React from "react";
+import logo from "../assets/Top.png";
+import userprofile from "../assets/user.svg";
 
 const Navbar = () => {
-  return (<>
-    {/* navbar main container */}
-    <nav className='w-screen bg-white h-28 flex justify-around items-center px-10 shadow-md gap-48' >
-      <img className='' src={logo} />
-      {/* Nav items */}
-      <div className='flex justify-between items-center gap-20 text-xl font-normal text-black'>
-        <span>Home</span>
-        <span>Request</span>
+  return (
+    <>
+      {/* navbar main container */}
+      <nav className="w-screen bg-white h-28 flex justify-around items-center px-10 shadow-md gap-48">
+        <img className="" src={logo} />
+        {/* Nav items */}
+        <div className="flex justify-between items-center gap-20 text-xl font-normal text-black">
+          <span>Home</span>
+          <span>Request</span>
 
-      <div className='flex gap-10 '>
-        <img src={userprofile} alt="userProfile" className='h-8 w-8 '/>
-      </div>
-      </div>
-    </nav>
-  </>
-  )
-}
+          <div className="flex gap-10 ">
+            <img src={userprofile} alt="userProfile" className="h-8 w-8 " />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
