5a654e1bd92107e69732632115d1dbb1a177a6db
Lizan7 <lizanstha777@gmail.com>
2024-07-29 14:04:07 +0545

    admin request css fixed

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -23,7 +23,7 @@ const Category = () => {
           <CategoryTable />
           <ItemCategoryTable />
           <ProductCategoryTable />
-          <Features/>
+          <Features />
         </div>
       </div>
     </div>
--- src/pages/Request.jsx
+++ src/pages/Request.jsx
@@ -11,12 +11,12 @@ const Request = () => {
       <Sidebar />
       <div className="flex flex-col gap-4 mx-auto  items-center">
         <Topbar />
-        <div className="flex flex-col bg-white w-[85.5vw] px-4 py-4 rounded gap-2 h-[88vh]">
+        <div className="flex flex-col bg-white w-[85.5vw] px-9 py-5 rounded gap-2 h-[88vh] ">
           <h3 className=" font-semibold text-2xl">Requests</h3>
           <p>You can view your request here</p>
-          <hr className="h-1 bg-blue-500 border-none"></hr>
-          <div className="h-[82vh] overflow-y-scroll mt-3">
-            <RequestTable />
+          <div className="w-[100%] mx-auto mt-5 bg-blue-600 h-1"></div>
+          <div className="h-[82vh] overflow-y-scroll mt-3 ">
+            <RequestTable  />
           </div>
         </div>
       </div>
