d174769329899bbdff13ebdefe1ea916a4ea447b
Lenisha Ghimire <lenishaghimire07@gmail.com>
2024-08-07 13:57:31 +0545

    all user and sctive user table

--- src/pages/SettingRole.jsx
+++ src/pages/SettingRole.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Topbar from "../components/Topbar";
-
 import axios from "axios";
+import ActiveUser from "../components/ActiveUser";
+import AllUser from "../components/AllUser";
+
 
 const SettingRole = () => {
   const [users, setUsers] = useState([]);
@@ -11,6 +13,20 @@ const SettingRole = () => {
 
   const Token = localStorage.getItem("token");
 
+
+
+  const cellStyle = {
+    fontSize: "14px",
+    padding: "12px 16px",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  };
+
+  const headerStyle = {
+    fontWeight: 600,
+    backgroundColor: "#f5f5f5",
+  };
   useEffect(() => {
     const getUsers = async () => {
       try {
@@ -41,73 +57,42 @@ const SettingRole = () => {
   return (
     <div className="w-screen h-screen flex justify-between bg-background">
       <Sidebar />
-      <div className="flex flex-col gap-4 mx-auto  items-center">
+      <div className="flex flex-col gap-4 mx-auto items-center">
         <Topbar />
-      
-            <div className="flex flex-col bg-white w-[85.5vw] px-9 py-5 rounded gap-2 h-[88vh]">
-            <h3 className="font-semibold text-2xl">Roles</h3>
-            <div className="flex items-center justify-between ">
-              <p>You can manage roles here</p>
-              <button className="bg-blue-600 text-white py-2 px-3 rounded">
-                Add Roles
-              </button>
-            </div>
-      <div className="w-full mx-auto mt-5 bg-blue-600 h-1"></div>
 
-              <div className="p-8">
-                <div className="relative overflow-x-auto flex justify-center items-center">
-                  <table className="w-[95%] text-sm text-center text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs border-b border-gray-200 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                      <tr>
-                        <th scope="col" className="px-10 py-3">
-                          S.No.
-                        </th>
-                        <th scope="col" className="px-10 py-3">
-                          User Name
-                        </th>
-                        <th scope="col" className="px-10 py-3">
-                          Email Address
-                        </th>
-                        <th scope="col" className="px-10 py-3">
-                          Role
-                        </th>
-                        <th scope="col" className="px-10 py-3">
-                          Action
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {users.map((user, index) => (
-                        <tr
-                          key={user.id}
-                          className="bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700"
-                        >
-                          <th
-                            scope="row"
-                            className="px-10 py-4 font-medium text-gray-900 whitespace-nowrap"
-                          >
-                            {index + 1}
-                          </th>
-                          <td className="px-10 py-4">{user.user_name}</td>
-                          <td className="px-10 py-4">{user.user_email}</td>
-                          <td className="px-10 py-4">{user.role}</td>
-                          <td className="px-10 py-4">
-                            <button>
-                              <i className="fa-solid fa-ellipsis-vertical"></i>
-                            </button>
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
+        <div className="flex flex-col bg-white w-[85.5vw] px-9 py-5 rounded gap-2 h-[88vh]">
+          <h3 className="font-semibold text-2xl">Roles</h3>
+          <div className="flex items-center justify-between ">
+            <p>You can manage roles here</p>
+            <button className="bg-blue-600 text-white py-2 px-3 rounded">
+              Add Roles
+            </button>
+          </div>
+          <div className="w-full mx-auto mt-5 bg-blue-600 h-1"></div>
+
+
+          <div className="flex flex-row gap-6">
+          <div className="flex w-fit p-7 justify-between border-2 border-neutral-300 rounded-md mt-3 text-l text-black font-semibold">
+            <div className="flex flex-col justify-between mb-6 gap-5">
+              <h1 className="text-lg font-bold m-2">Active Users</h1>
+              <div className="relative  overflow-x-auto flex justify-center items-center">
+                <ActiveUser users={users} />
               </div>
             </div>
+          </div>
           
-      </div>{" "}
-      {/* End of flex-grow div */}
+          <div className="flex w-fit p-7 justify-between border-2 border-neutral-300 rounded-md mt-3 text-l text-black font-semibold">
+            <div className="flex flex-col justify-between mb-6 gap-5">
+              <h1 className="text-lg font-bold m-2">All Users</h1>
+              <div className="relative  overflow-x-auto flex justify-center items-center">
+                <AllUser users={users} />
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
     </div>
-    
   );
 };
 
