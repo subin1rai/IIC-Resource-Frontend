5fb5d9621e46a87b94241d5d6711483925a08755
Lizan7 <lizanstha777@gmail.com>
2024-08-14 12:35:50 +0545

    Userhome modfied

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,13 @@ import notificationIcon from "../assets/notification.svg"
 const Navbar = () => {
   return (
     <nav className="flex items-center w-screen p-5 justify-around">
-      <img src={logo} alt=""/>
+      <img src={logo} alt="" />
       {/* Nav items */}
       <div className="flex gap-14 text-lg px-10 font-medium">
         <p>Home</p>
-        <p>How to?</p>
         <p>Request</p>
         <p>History</p>
+        <p>How to?</p>
       </div>
       <div className="flex gap-4">
         <img src={notificationIcon} alt="" />
