f2181cf512194fc4d8d97884abe2a6a7656ac72c
Lizan7 <lizanstha777@gmail.com>
2024-08-22 14:18:25 +0545

    Some small changes

--- src/components/Featurestable.jsx
+++ src/components/Featurestable.jsx
@@ -11,7 +11,7 @@ const Featurestable = ({feature}) => {
             <div className='w-[80%] py-4'>
               <div className='flex w-full justify-between '>
                 <p className='text-lg'>{feat.feature_name}</p>
-                <img src={edit} alt="" className='h-5 w-5' />
+                <img src={edit} alt="" className='h-5 w-5' onClick={editCategoryform}/>
               </div>
             </div>
             <hr className='h-1 w-[85%] border-button' />
