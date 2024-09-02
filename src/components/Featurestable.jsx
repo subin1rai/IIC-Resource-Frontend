0f331e33cefd433464a5bd76e9dbdec59e7e1ff0
Lizan7 <lizanstha777@gmail.com>
2024-09-02 13:39:54 +0545

    category empty state fixed

--- src/components/Featurestable.jsx
+++ src/components/Featurestable.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import edit from "../assets/editIcon.png";
+import empty from "../assets/Emptycate.svg"
+
 
 const Featurestable = ({
   feature,
@@ -16,22 +18,31 @@ const Featurestable = ({
   };
   return (
     <div className="flex flex-col bg-white items-center pb-4 rounded-b-md overflow-auto ">
-      {feature.map((feat) => (
-        <div className="w-full flex flex-col items-center">
-          <div className="w-[80%] py-4">
-            <div className="flex w-full justify-between items-center">
-              <p className="text-lg">{feat.feature_name}</p>
-              <img
-                src={edit}
-                alt=""
-                className="h-5 w-5 cursor-pointer"
-                onClick={() => handleformVisiblity(feat)}
-              />
+      {feature.length > 0 ? (
+
+        feature.map((feat) => (
+          <div className="w-full flex flex-col items-center">
+            <div className="w-[80%] py-4">
+              <div className="flex w-full justify-between items-center">
+                <p className="text-lg">{feat.feature_name}</p>
+                <img
+                  src={edit}
+                  alt=""
+                  className="h-5 w-5 cursor-pointer"
+                  onClick={() => handleformVisiblity(feat)}
+                />
+              </div>
             </div>
+            <hr className="h-1 w-[85%] border-button" />
           </div>
-          <hr className="h-1 w-[85%] border-button" />
+        ))
+      ) : (
+        <div className="flex flex-col items-center justify-center py-8">
+          <img src={empty} alt="No categories" className="h-32 w-32 mb-4" />
+          <p className="text-gray-500">No features added yet !</p>
         </div>
-      ))}
+      )
+      }
     </div>
   );
 };
--- src/components/ItemCategoryTable.jsx
+++ src/components/ItemCategoryTable.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import edit from "../assets/editIcon.png";
+import empty from "../assets/Emptycate.svg"
+
 
 const ItemCategoryTable = ({
   itemCategory,
@@ -15,22 +17,30 @@ const ItemCategoryTable = ({
   };
   return (
     <div className="flex flex-col bg-white items-center pb-4 rounded-b-md overflow-auto ">
-      {itemCategory.map((iCat) => (
-        <div className="w-full flex flex-col items-center">
-          <div className="w-[80%] py-4">
-            <div className="flex w-full justify-between items-center ">
-              <p className="text-lg">{iCat.item_category_name}</p>
-              <img
-                src={edit}
-                alt=""
-                className="h-5 w-5 cursor-pointer"
-                onClick={() => handleformVisiblity(iCat)}
-              />
+      {itemCategory.length > 0 ? (
+        itemCategory.map((iCat) => (
+          <div className="w-full flex flex-col items-center">
+            <div className="w-[80%] py-4">
+              <div className="flex w-full justify-between items-center ">
+                <p className="text-lg">{iCat.item_category_name}</p>
+                <img
+                  src={edit}
+                  alt=""
+                  className="h-5 w-5 cursor-pointer"
+                  onClick={() => handleformVisiblity(iCat)}
+                />
+              </div>
             </div>
+            <hr className="h-1 w-[85%] border-button" />
           </div>
-          <hr className="h-1 w-[85%] border-button" />
+        ))
+      ) : (
+        <div className="flex flex-col items-center justify-center py-8">
+          <img src={empty} alt="No categories" className="h-32 w-32 mb-4" />
+          <p className="text-gray-500">No categories added yet !</p>
         </div>
-      ))}
+      )
+      }
     </div>
   );
 };
