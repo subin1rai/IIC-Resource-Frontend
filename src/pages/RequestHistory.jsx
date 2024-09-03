50fc4878218b4d93f41ef3ed46f37f3cf4a3f453
Lenisha Ghimire <lenishaghimire07@gmail.com>
2024-09-03 10:02:04 +0545

    User Request

--- src/pages/RequestHistory.jsx
+++ src/pages/RequestHistory.jsx
@@ -6,43 +6,53 @@ const RequestHistory = () => {
     <div className="h-screen w-screen">
       <Navbar />
       <div className="flex flex-col items-center h-[87.5vh]">
-        
         <div className="w-[80%] p-8 gap-3">
           <h1 className="font-medium text-2xl">History</h1>
           <p>You can view your request here.</p>
           <hr className="mt-4 border-2 border-blue-600" />
         </div>
+
         
-        
-        <div className="flex flex-col w-[80%] justify-between">
-          <div className="flex flex-col bg-gray w-[50%] p-5 rounded-lg ml-8 gap-4">
-            <h1 className="text-lg font-bold">Charger</h1>
-                <div className='flex flex-col gap-4'>
-                  <p className='font-medium'>Issued item:</p>
-                  <p className='font-medium'>Quantity:</p>
+          <div className="flex flex-col w-[80%]">
+            <div className="flex flex-wrap gap-8">
+            <div className="flex flex-col bg-gray w-[47%] p-5 rounded-lg gap-4 ml-7">
+                  <h1 className="text-lg font-bold">Charger</h1>
+                  
+                  <div className='flex justify-between'>
+                    <div className="flex flex-col gap-4">
+                      <p className='font-medium'>Issued item:</p>
+                      <p className='font-medium'>Quantity:</p>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                      <p className='font-medium'>Requested for:</p>
+                      <p className='font-medium'>Purpose:</p>
+                    </div>
+                    <div className="flex justify-center items-center">
+                      Approved
+                    </div>
+                  </div>
                 </div>
 
-                <div className='flex flex-col gap-4'>
-                  <p className='font-medium'>Requested For:</p>
-                  <p className='font-medium'>Purpose:</p>
+                <div className="flex flex-col bg-gray w-[47%] p-5 rounded-lg gap-4 ml-7">
+                  <h1 className="text-lg font-bold">Charger</h1>
+                  
+                  <div className='flex justify-between'>
+                    <div className="flex flex-col gap-4">
+                      <p className='font-medium'>Issued item:</p>
+                      <p className='font-medium'>Quantity:</p>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                      <p className='font-medium'>Requested for:</p>
+                      <p className='font-medium'>Purpose:</p>
+                    </div>
+                    <div className="flex justify-center items-center">
+                      Pending
+                    </div>
+                  </div>
                 </div>
+
+              </div>
             </div>
-
-
-            <div className="flex flex-col bg-gray w-[50%] p-5 rounded-lg ml-8 gap-4">
-            <h1 className="text-lg font-bold">Charger</h1>
-                <div className='flex flex-col gap-4'>
-                  <p className='font-medium'>Issued item:</p>
-                  <p className='font-medium'>Quantity:</p>
-                </div>
-
-                <div className='flex flex-col gap-4'>
-                  <p className='font-medium'>Requested For:</p>
-                  <p className='font-medium'>Purpose:</p>
-                </div>
-            </div>
-            </div>
-        
         
         </div>
 
