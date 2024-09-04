bbd5263d8da7f91488f73ab11a5eaeea39b039d6
Mahiimaa <137261863+Mahiimaa@users.noreply.github.com>
2024-09-04 11:19:18 +0545

    user history

--- src/pages/RequestHistory.jsx
+++ src/pages/RequestHistory.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import {useState, useEffect} from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 const RequestHistory = () => {
+      const [approvedHistory, setApprovedHistory] = useState([]);
+      const [pendingHistory, setPendingHistory] = useState ([]);
+
+      
+  const userInfo = useSelector((state) => state.user.userInfo);
+  const token = userInfo.token;
+   
+  useEffect(() => {
+    const getHistory = async () => {
+      try {
+        const response = await axios.get("http://localhost:8898/api/requestHistory", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          } 
+        });
+        console.log(response)
+        setApprovedHistory(response.data.requestApproved || []);
+        setPendingHistory(reponse.data.requestPending || []);
+      }
+      catch (error){
+        console.log("Error fetching history:", error);
+        setApprovedHistory([]);
+        setPendingHistory([]);
+      }
+      };
+      getHistory();
+    }, [token])   
 
 
   return (
@@ -16,33 +46,67 @@ const RequestHistory = () => {
 
         <div className="flex flex-col w-[78%]">
           <div className="flex flex-wrap gap-8 ml-9">
-            <div className="flex flex-col bg-gray w-[48%] p-5 rounded-lg gap-4">
-              <h1 className="text-lg font-bold">Charger</h1>
+          {history.map((item, index) => (
+            <div key= {index} className="flex flex-col bg-gray w-[48%] p-5 rounded-lg gap-4">
+              <h1 className="text-lg font-bold">{item.requestItems}</h1>
 
               <div className="flex justify-between">
                 <div className="flex flex-col gap-4">
-                  <p className="font-medium">Issued item:</p>
-                  <p className="font-medium">Quantity:</p>
+                  <p className="font-medium">Issued item:
+                  <span className="font-normal  pl-4">
+                    {item?.issued_item|| 0}
+                  </span>
+                  </p>
+                  <p className="font-medium">Quantity:
+                  <span className="font-normal  pl-4">
+                    {history?.requested_for|| 0}
+                  </span>
+                  </p>
                 </div>
                 <div className="flex flex-col gap-4">
-                  <p className="font-medium">Requested for:</p>
-                  <p className="font-medium">Purpose:</p>
+                  <p className="font-medium">Requested for:
+                  <span className="font-normal  pl-4">
+                    {history?.requested_for|| 0}
+                  </span>
+                  </p>
+                  <p className="font-medium">Purpose:
+                  <span className="font-normal  pl-4">
+                    {history?.requested_for|| 0}
+                  </span>
+                  </p>
                 </div>
                 <div className="flex justify-center items-center">Approved</div>
               </div>
             </div>
+          ))}
 
             <div className="flex flex-col bg-gray w-[48%] p-5 rounded-lg gap-4">
               <h1 className="text-lg font-bold">Charger</h1>
 
               <div className="flex justify-between">
                 <div className="flex flex-col gap-4">
-                  <p className="font-medium">Issued item:</p>
-                  <p className="font-medium">Quantity:</p>
+                  <p className="font-medium">Issued item:
+                  <span className="font-normal  pl-4">
+                    {history?.requested_for|| 0}
+                  </span>
+                  </p>
+                  <p className="font-medium">Quantity:
+                  <span className="font-normal  pl-4">
+                    {history?.requested_for|| 0}
+                  </span>
+                  </p>
                 </div>
                 <div className="flex flex-col gap-4">
-                  <p className="font-medium">Requested for:</p>
-                  <p className="font-medium">Purpose:</p>
+                  <p className="font-medium">Requested for:
+                  <span className="font-normal  pl-4">
+                    {history?.requested_for|| 0}
+                  </span>
+                  </p>
+                  <p className="font-medium">Purpose:
+                  <span className="font-normal  pl-4">
+                    {history?.requested_for|| 0}
+                  </span>
+                  </p>
                 </div>
                 <div className="flex justify-center items-center">Pending</div>
               </div>
